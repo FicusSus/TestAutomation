b72a828dba1a641aee97dfd37ea1178257420721
Kira Solovjova <solovjova467@gmail.com>
2026-04-08 18:01:58 +0300

    Check

---
+++ JavaActivities/src/main/java/com.bootcampexercise.module1/VariableAssignmentActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexercise.module1;/* HelloWorld.java
+ */
+
+public class VariableAssignmentActivity
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
